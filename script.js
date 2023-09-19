6c970ca357903bb420d50e34a994cdd57a8e01a9
Raphael Santos Lopes Da Silva <105462948+RaphaelSilvaL@users.noreply.github.com>
2023-09-19 11:09:26 -0300

    Update script.js 1

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var button = document.getElementById('meu-botao');
 var imageContainer = document.getElementById("imageContainer");
 var audio = document.getElementById('audio');
 
-
 button.addEventListener("click", () => {
     audio.play();
     button.style.zIndex = 0;
