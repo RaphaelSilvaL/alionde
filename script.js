8177100479d03e3cbe1439939d268be508a7b6ad
Raphael Santos Lopes Da Silva <105462948+RaphaelSilvaL@users.noreply.github.com>
2023-09-19 10:58:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-var button = document.getElementById('meu-botao')
-var imageContainer = document.getElementById("imageContainer")
+var button = document.getElementById('meu-botao');
+var imageContainer = document.getElementById("imageContainer");
+var audio = document.getElementById('audio');
 
-button.addEventListener("click", ()=> {
-    audio.play(); 
+button.addEventListener("click", () => {
+    audio.play();
     button.style.zIndex = 0;
     setTimeout(() => {
         imageContainer.style.opacity = 1;
-    }, 100); 
+        imageContainer.querySelector('img').style.transform = 'translateX(-50%) rotate(2520deg)'; // Girar 3 vezes
+    }, 100);
 });
